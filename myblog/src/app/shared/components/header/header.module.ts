b54eb274db2449eb71107bc860073853b1156391
RudyMontenegro <rudymontenegro@gmail.com>
2021-03-16 23:20:43 -0400

    agregando navbar jeje

---
+++ myblog/src/app/shared/components/header/header.module.ts
@@ -0,0 +1,13 @@
+import { HeaderComponent } from './header.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeaderModule { }
